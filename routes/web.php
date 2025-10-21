6f82c5f59e74c8cb5487b3dc1fa73c66f4772e6a
Jose Rea <jose.rea@uees.edu.ec>
2025-10-20 22:34:53 -0500

    Merge login Admin

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,5 @@ Route::get('/admin/dashboard', function () {
     return view('admin');
 })->name('admin.dashboard')->middleware('auth');
 
+Route::get('/admin/login', function () { return view('adminLogin'); })->name('admin.login');
+Route::post('/admin/login', [AuthController::class, 'adminLogin'])->name('admin.login.submit'); 
